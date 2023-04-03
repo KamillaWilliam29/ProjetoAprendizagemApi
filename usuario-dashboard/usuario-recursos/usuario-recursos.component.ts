06adb064b73779701b8629d4e4ed454fbce37308
Kamilla <51264700+KamillaWilliam29@users.noreply.github.com>
2023-04-03 20:46:51 -0300

    Distribuímos as pastas do projeto Angular em 3 partes.
    Descrevemos as pastas e dividimos o Projeto AngularJS da Aprendizagem.Api em 3 partes.

---
+++ usuario-dashboard/usuario-recursos/usuario-recursos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Recurso } from 'src/app/models/Recurso';
+import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { RecursoService } from 'src/app/services/recurso.service';
+
+@Component({
+  selector: 'app-usuario-recursos',
+  templateUrl: './usuario-recursos.component.html',
+  styleUrls: ['./usuario-recursos.component.css']
+})
+export class UsuarioRecursosComponent implements OnInit {
+
+  recursos: Recurso[];
+  arquivosUpload: any[] = [];
+  loading: boolean = true;
+  idUsuarioLogado: string;
+  maxFileSize: number = 1000000;
+
+
+  constructor(
+    private recursoService: RecursoService,
+    private authGuardService: AuthGuardService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.idUsuarioLogado = this.authGuardService.getIdUsuarioLogado();
+
+    this.recursoService.ObterRecursoPeloUsuarioId(this.idUsuarioLogado).subscribe(resultado => {
+      this.recursos = resultado;
+    })
+  }
+
+  onUpload(event: any) {
+    for (let file of event.files) {
+      this.arquivosUpload.push(file);
+    }
+  }
+
+  onSave(event: any) {
+    console.log(event);
+  }
+
+}
